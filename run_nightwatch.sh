87fe26db95eee8b644fd4930c638afc1966aebe8
Abigail Watson <awatson1978@gmail.com>
2014-09-19 23:59:12 -0400

    moved tests to tests/nightwatch

--- run_nightwatch.sh
+++ run_nightwatch.sh
@@ -4,8 +4,8 @@ chmod +x .meteor/local/build/programs/server/assets/packages/clinical_nightwatch
 
 echo "installing nightwatch in .meteor/local/build"
   cd .meteor/local/build
-  sudo npm install nightwatch@0.5.3
+  npm install nightwatch@0.5.3
   cd ../../..
 
 echo "running nightwatch"
-   sudo .meteor/local/build/node_modules/nightwatch/bin/nightwatch -c .meteor/local/build/programs/server/assets/packages/clinical_nightwatch/nightwatch_from_app_root.json $1 $2
+  .meteor/local/build/node_modules/nightwatch/bin/nightwatch -c .meteor/local/build/programs/server/assets/packages/clinical_nightwatch/nightwatch_from_app_root.json $1 $2
